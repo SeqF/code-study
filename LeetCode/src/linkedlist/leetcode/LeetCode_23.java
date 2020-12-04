875a9955c97506db8d80d3e4f0dff0ee3fc78508
paksu.fang <Paksu.BS.Fang@cityline.com.hk>
2020-12-04 14:12:03 +0800

    update 20.12.4

--- LeetCode/src/linkedlist/leetcode/LeetCode_23.java
+++ LeetCode/src/linkedlist/leetcode/LeetCode_23.java
@@ -1,32 +1,31 @@
 package linkedlist.leetcode;
 
 import linkedlist.base.Node;
-import linkedlist.base.SingleLinkedList;
 
 /**
  * 给你一个链表数组，每个链表都已经按升序排列。
  * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
- *
+ * <p>
  * 示例 1：
  * 输入：lists = [[1,4,5],[1,3,4],[2,6]]
  * 输出：[1,1,2,3,4,4,5,6]
  * 解释：链表数组如下：
  * [
- *   1->4->5,
- *   1->3->4,
- *   2->6
+ * 1->4->5,
+ * 1->3->4,
+ * 2->6
  * ]
  * 将它们合并到一个有序链表中得到。
  * 1->1->2->3->4->4->5->6
- *
+ * <p>
  * 示例 2：
  * 输入：lists = []
  * 输出：[]
- *
+ * <p>
  * 示例 3：
  * 输入：lists = [[]]
  * 输出：[]
- *
+ * <p>
  * 题目链接:https://leetcode-cn.com/problems/merge-k-sorted-lists/comments/
  */
 public class LeetCode_23 {
@@ -35,61 +34,65 @@ public class LeetCode_23 {
      * 多路归并
      * 参考文章（外部排序）:http://blog.itpub.net/31561269/viewspace-2564096/
      * 参考文章（priority_queue）:https://www.cnblogs.com/fightingcode/p/11616969.html
-     * 思路：直接对Node[] lists 进行归并？
+     * 思路：
+     *    直接对NodeLists数组进行归并，但是最后的merge是对链表的merge
+     *
      * @param lists
      * @return
      */
-    public SingleLinkedList mergeKLists(Node[] lists){
+    public Node mergeKLists(Node[] lists) {
 
-        for (int i = 0; i < lists.length; i++) {
-
+        if(lists==null||lists.length==0){
+            return null;
         }
-        return null;
+        return merge(lists,0,lists.length-1);
     }
 
-    public void merge(Node[] a,int low,int mid,int high){
+    public Node merge(Node[] lists,int low,int high){
 
-        Node list = a[0];
-
-        Node temp = new Node();
-
-        if(list.next!=null){
-            temp.next=list.next;
-            list.next=null;
-        }
-
-        int count=0;
-        Node p=temp.next;
-        Node q=temp.next;
-        Node m;
-        Node r=list;
-        while (p.next!=null){
-            p=p.next;
-            count++;
-            if(count==mid){
-                m=p;
-            }
-            if(count>mid){
-                if(q.data<=p.data){
-                    Node node = new Node();
-                    node.data=q.data;
-                    node.next=null;
-                    r.next=node;
-                }else {
-                    Node node=new Node();
-                    node.data=p.data;
-                    node.next=null;
-                    r.next=node;
-                }
-            }
-        }
-
-        while (q!=m){
-            m.next=null;
-            r.next=q;
-        }
-
-        while (p)
-
+        int mid=low+(high-low)/2;
+        Node list1=merge(lists,low,mid);
+        Node list2=merge(lists,mid+1,high);
+        return mergeTwoList(list1,list2);
     }
+
+    public Node mergeTwoList(Node l1, Node l2) {
+
+        if (l1.next == null) {
+            return l2;
+        }
+        if (l2.next == null) {
+            return l1;
+        }
+
+        Node p = l1;
+        Node q = l2;
+        Node l3 = new Node();
+        Node r = l3;
+        while (p.next != null && q.next != null) {
+
+            if (p.data < q.data) {
+                r.next = p;
+                p = p.next;
+
+                r = r.next;
+                r.next = null;
+            } else {
+                r.next = q;
+                q = q.next;
+
+                r = r.next;
+                r.next = null;
+            }
+        }
+
+        if (p.next != null) {
+            r.next = p;
+        }
+        if (q.next != null) {
+            r.next = q;
+        }
+        return l3;
+    }
+
 }
