21e472088b2f925befb322a7761cf54085352704
boss <505475064@qq.com>
2021-03-16 23:39:26 +0800

    update
    Signed-off-by: boss <505475064@qq.com>

--- LeetCode/src/search/BinarySearch.java
+++ LeetCode/src/search/BinarySearch.java
@@ -130,12 +130,42 @@ public class BinarySearch {
         return -1;
     }
 
-    public int bSearch3() {
-
+    //变体三：查找第一个大于等于给定值的元素
+    public int bSearch3(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] >= value) {
+                if ((mid == 0) || (a[mid - 1] < value)) {
+                    return mid;
+                } else {
+                    high = mid - 1;
+                }
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
     }
 
-    public int bSearch4() {
-
+    //变体四：查找最后一个小于等于给定值的元素
+    public int bSearch4(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] <= value) {
+                if ((mid == n - 1) || a[mid + 1] <= value) {
+                    return mid;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
     }
 
 
