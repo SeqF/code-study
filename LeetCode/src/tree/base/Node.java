78f1e4d0db81422a388b9ef3a11e91fe23ebfcf7
paksu.fang <Paksu.BS.Fang@cityline.com.hk>
2021-03-24 17:55:02 +0800

    update

--- LeetCode/src/tree/base/Node.java
+++ LeetCode/src/tree/base/Node.java
@@ -5,12 +5,22 @@ package tree.base;
  */
 public class Node {
 
-    int value;
-    Node left;
-    Node right;
-    Node parent;
+    public int value;
+    public Node left;
+    public Node right;
+    public Node parent;
 
-    Node(int value) {
+    public Node(int value) {
         this.value = value;
+    }
+
+    public Node(int value,Node left,Node right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+
+    public Node() {
+
     }
 }
