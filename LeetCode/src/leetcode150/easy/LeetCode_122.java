0e4338bbb15ca9e085aade12b7ab1fdcb575aa25
paksu.fang <Paksu.BS.Fang@cityline.com.hk>
2021-04-02 17:58:53 +0800

    update

--- LeetCode/src/leetcode150/easy/LeetCode_122.java
+++ LeetCode/src/leetcode150/easy/LeetCode_122.java
@@ -1,16 +1,32 @@
 package leetcode150.easy;
 
 /**
- *
  * 买卖股票的最佳时机 II
  *
  * @author paksu
- *
+ * <p>
  * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
  */
 public class LeetCode_122 {
 
+    /**
+     * 当天卖出的股票，当天还能买入，所以利润是每天叠加
+     * 相当于涨价，在一定时间内的涨价相当于每天的涨幅加起来之和，不用管是在第几天进行购买和抛售
+     *
+     * @param prices
+     * @return
+     */
     public int maxProfit(int[] prices) {
-
+        int length = prices.length;
+        if (length <= 1) {
+            return 0;
+        }
+        int profit = 0;
+        for (int i = 1; i < length; i++) {
+            if (prices[i - 1] < prices[i]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
     }
 }
--- LeetCode/src/leetcode150/easy/LeetCode_412.java
+++ LeetCode/src/leetcode150/easy/LeetCode_412.java
@@ -1,2 +1,36 @@
-package leetcode150.easy;public class LeetCode_412 {
+package leetcode150.easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Fizz Buzz
+ *
+ * @author paksu
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/fizz-buzz/
+ */
+public class LeetCode_412 {
+
+    /**
+     * 注意同时3和5的倍数要先判断
+     * @param n
+     * @return
+     */
+    public List<String> fizzBuzz(int n) {
+        String[] list = new String[n];
+        for (int i = 0; i < list.length; i++) {
+            if ((i+1) % 3 == 0 && (i+1) % 5 == 0) {
+                list[i] = "FizzBuzz";
+            } else if ((i+1) % 5 == 0) {
+                list[i] = "Buzz";
+            } else if ((i+1) % 3 == 0 ) {
+                list[i] = "Fizz";
+            } else {
+                list[i] = Integer.toString(i+1);
+            }
+        }
+        return Arrays.asList(list);
+    }
 }
