1e84b1f29a6774f9935b3f9eaf86e9a8cf075487
paksu.fang <Paksu.BS.Fang@cityline.com.hk>
2021-04-12 17:10:29 +0800

    update

--- LeetCode/src/leetcode150/easy/LeetCode_118.java
+++ LeetCode/src/leetcode150/easy/LeetCode_118.java
@@ -13,6 +13,8 @@ public class LeetCode_118 {
 
     /**
      * 终极无敌坑的List<List<Integer>>!!!
+     * 二维链表的初始化与遍历：https://www.baeldung.com/java-multi-dimensional-arraylist
+     *
      * 先想象构造成numRows x numRows 的矩阵，然后取矩阵的下半部分，即i>=j的下三角部分
      * 1、当j=0时或者i=j时，数组元素为1
      * 2、a[i][j]=a[i-1][j-1]+a[i-1][j]，即任意位置的元素等于上列左边的元素和正上方的元素之和(在直角情况下观察）
@@ -26,14 +28,14 @@ public class LeetCode_118 {
         List<List<Integer>> arr = new ArrayList<>(numRows);
         for (int i = 0; i < numRows; i++) {
             //下三角的每个数组长度都为对应的i+1
-            arr.add(i, new ArrayList<>(i + 1));
+            arr.add(new ArrayList<>(i + 1));
         }
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < i + 1; j++) {
                 if (j == 0 || j == i) {
-                    arr.get(i).add(j, 1);
+                    arr.get(i).add(1);
                 } else {
-                    arr.get(i).add(j, arr.get(i - 1).get(j - 1) + arr.get(i - 1).get(j));
+                    arr.get(i).add(arr.get(i - 1).get(j - 1) + arr.get(i - 1).get(j));
                 }
             }
         }
