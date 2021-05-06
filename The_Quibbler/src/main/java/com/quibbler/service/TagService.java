9403b9ed63135e691cf562a4c7d5e20d731d1fde
paksu.fang <Paksu.BS.Fang@cityline.com.hk>
2021-05-06 16:15:13 +0800

    update

--- The_Quibbler/src/main/java/com/quibbler/service/TagService.java
+++ The_Quibbler/src/main/java/com/quibbler/service/TagService.java
@@ -1,11 +1,11 @@
 package com.quibbler.service;
 
-import com.quibbler.entity.Tag;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.quibbler.model.entity.Tag;
 
 /**
  * <p>
- *  服务类
+ * 服务类
  * </p>
  *
  * @author Paksu
