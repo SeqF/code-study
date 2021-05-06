785a7a88e1f8fdddf9558582e6cdfd3c1a766288
paksu.fang <Paksu.BS.Fang@cityline.com.hk>
2021-05-06 15:56:46 +0800

    update

--- LeetCode/src/leetcode150/medium/LeetCode_78.java
+++ LeetCode/src/leetcode150/medium/LeetCode_78.java
@@ -17,25 +17,16 @@ public class LeetCode_78 {
     List<Integer> path = new ArrayList<>();
 
     public List<List<Integer>> subsets(int[] nums) {
-        result.add(new ArrayList<>());
         backTracking(nums, 0);
         return result;
     }
 
     private void backTracking(int[] nums, int startIndex) {
-        if (path.size() < nums.length) {
-            result.add(new ArrayList<>(path));
-            return;
-        }
+        result.add(new ArrayList<>(path));
         for (int i = startIndex; i < nums.length; i++) {
             path.add(nums[i]);
             backTracking(nums, i + 1);
             path.remove(Integer.valueOf(nums[i]));
         }
-    }
-
-    public static void main(String[] args) {
-        LeetCode_78 code78 = new LeetCode_78();
-        System.out.println(code78.subsets(new int[]{1, 2, 3}).toString());
     }
 }
