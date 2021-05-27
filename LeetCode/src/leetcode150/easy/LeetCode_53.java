67d07b4df8d0924d84a176329522b085a88a8b87
paksu.fang <Paksu.BS.Fang@cityline.com.hk>
2021-05-27 10:06:08 +0800

    update

--- LeetCode/src/leetcode150/easy/LeetCode_53.java
+++ LeetCode/src/leetcode150/easy/LeetCode_53.java
@@ -9,22 +9,11 @@ package leetcode150.easy;
  */
 public class LeetCode_53 {
 
-    /**
-     * 用动态规划解决
-     * 1、状态：dp[i]表示到nums[i]结尾的最大的和
-     * 2、状态转移方程：dp[i]=Math.max(dp[i-1]+nums[i],nums[i])
-     * 3、初始化：dp[0]=nums[0]，第一序列之和和为数组第一个
-     * 4、输出：返回最大值
-     *
-     * @param nums
-     * @return
-     */
-    public int mxaSubArray(int[] nums) {
+    public int maxSubArray(int[] nums) {
         int length = nums.length;
-        //构建动态规划数组
         int[] dp = new int[length + 1];
-        int result = Integer.MIN_VALUE;
         dp[0] = nums[0];
+        int result = Integer.MIN_VALUE;
         for (int i = 1; i < length; i++) {
             dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
             if (dp[i] > result) {
@@ -32,5 +21,11 @@ public class LeetCode_53 {
             }
         }
         return Math.max(dp[0], result);
+    }
+
+    public static void main(String[] args) {
+        LeetCode_53 code53 = new LeetCode_53();
+        int[] nums = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(code53.maxSubArray(nums));
     }
 }
