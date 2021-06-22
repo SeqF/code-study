e85fa0965b6e4208cda55c285ab02f34a4ecfadf
boss <505475064@qq.com>
2021-06-22 23:12:27 +0800

    update
    Signed-off-by: boss <505475064@qq.com>

---
+++ LeetCode/src/leetcode150/medium/LeetCode_289.java
@@ -0,0 +1,15 @@
+package leetcode150.medium;
+
+/**
+ * 生命游戏
+ *
+ * @author paksu
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/game-of-life/
+ */
+public class LeetCode_289 {
+
+    public void gameOfLife(int[][] board) {
+
+    }
+}
