4529010daec3d06e7591e84a69c5292561789134
paksu.fang <Paksu.BS.Fang@cityline.com.hk>
2021-07-23 15:45:43 +0800

    update

--- LeetCode/src/leetcode150/medium/LeetCode_341.java
+++ LeetCode/src/leetcode150/medium/LeetCode_341.java
@@ -1,5 +1,6 @@
 package leetcode150.medium;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.Consumer;
@@ -14,5 +15,37 @@ import java.util.function.Consumer;
  */
 public class LeetCode_341 {
 
+    /**
+     * 将list转化为N叉树，对N叉树进行前序深度优先遍历
+     * N叉树的前序遍历，类比二叉树，就是将孩子节点递归进入
+     */
+    public class NestedIterator implements Iterator<Integer> {
+
+        int cur = 0;
+        List<Integer> path = new ArrayList<>();
+        public NestedIterator(List<NestedInteger> nestedList) {
+            dfs(nestedList);
+        }
+
+        @Override
+        public Integer next() {
+            return path.get(cur++);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return cur!=path.size();
+        }
+
+        private void dfs(List<NestedInteger> list){
+            for(int i = 0;i<list.size();i++){
+                if(list.get(i).isInteger()){
+                    path.add(list.get(i).getInteger());
+                }else{
+                    dfs(list.get(i).getList());
+                }
+            }
+        }
+    }
 
 }
